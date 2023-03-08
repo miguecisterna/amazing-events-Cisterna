3c420437f6778efabcde20904bca7af582098bc0
Migue Cisterna <miguecisterna@gmail.com>
2023-03-08 11:17:17 -0300

    Implementada la busqueda por CheckBoxes

--- scripts/script.js
+++ scripts/script.js
@@ -8,7 +8,8 @@ let filteredArr = [];
 
 //Filter by categories variables:
 let selectedCategories = [];
-
+let checkedCategories = [];
+let checkedRefe = document.getElementById("allCategoriesContainer");
 
 //Categories Functions
 function createNoRepeatCategories(){
@@ -20,14 +21,63 @@ function createNoRepeatCategories(){
   }
 }
 
+function createCheckedCategoryCards(arr){
+  allCards="";
+  allCardsContainer.innerHTML = "";
+  for(event_ of eventsDB.events){
+    if(arr.includes(event_.category)){
+      allCards += 
+      `<div class="col-sm-12 col-md-6 col-lg-6 col-xl-4 cardCont">
+      <div class="card">
+        <img src="${event_.image}">
+        <div class="card-body">
+          <h5 class="card-title">${event_.name}</h5>
+          <p class="card-text">${event_.description}</p>
+          <div class="cardFooter">
+            <p>Price: $${event_.price}</p>
+            <a href="./details.html" class="btn btn-primary card-btn">More Info</a>
+          </div>
+        </div>
+      </div>
+    </div>`;
+    }
+  }
+}
+
+checkedRefe.addEventListener("click",(e) => {
+
+  if(e.target.attributes.class.nodeValue === "checkbox"){
+
+    if(e.target.checked){
+      checkedCategories.push(e.target.value);
+      createCheckedCategoryCards(checkedCategories);
+      paintAllCards();
+    }
+    else{
+      let index = checkedCategories.indexOf(e.target.value);
+      let x = checkedCategories.splice(index, 1);
+      createCheckedCategoryCards(checkedCategories);
+      paintAllCards();
+    }
+    console.log(checkedCategories);
+  }
+  if(checkedCategories.length === 0){
+    createAllCards(eventsDB.events);
+    paintAllCards();
+  }
+
+})
+
 function paintCategories(){
   allCategoriesContainer.innerHTML = allCategoriesHTML;
 }
 
 function createCategoriesSelected(){
-  for(category of noRepeatCategories){
-    selectedCategories.push(category);
-  }
+
+    for(category of noRepeatCategories){
+      selectedCategories.push(category);
+    }
+    
   console.log([selectedCategories]);
 }
 
@@ -77,13 +127,23 @@ function filtraPorNombre(nombre,eventos){
   return lista;
 }
 
+// function checkboxFilter(lista, evento){
+//   let lista = [];
+
+//   for(event_ of evento){
+//     if(evento.category.toLowerCase().includes(lista)){
+
+//     }
+//   }
+// }
+
 search.addEventListener("keyup", () =>{
 
   createAllCards(filtraPorNombre(search.value.toLowerCase(), eventsDB.events));
   paintAllCards();
 
 
-})
+});
 
 //Calling the functions:
 
@@ -94,9 +154,5 @@ createNoRepeatCategories();
 paintCategories();
 
 createCategoriesSelected();
-
-
-
-
 
 
